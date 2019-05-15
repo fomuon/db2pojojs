096277707c6c9c0b54ab077927b34c703604b76f
yongkyu.lee <fomuon@gmail.com>
2019-05-15 23:28:49 +0900

    commander 를 통해 arguments 구성

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the outside!');
+var commander = require('commander');
+
+function commaSeparatedList(value, dummyPrevious) {
+  return value.split(',');
+}
+
+commander
+  .name('db2pojojs')
+  .option('--mysql-host <mysql-host>', 'mysql host name')
+  .option('--mysql-port <mysql-port>', 'mysql port. default 3306', 3306)
+  .option('--mysql-user <mysql-user>', 'mysql user name')
+  .option('--mysql-password <mysql-password>', 'mysql password')
+  .option('--mysql-charset <mysql-charset>', 'mysql connection charset. default utf8', 'utf8')
+  .option('--db-name <db-name>', 'db name to generate pojo class')
+  .option('--table-names <table-names>', 'specific table names to generate pojo class. tab1,tab2... default all tables', commaSeparatedList)
+  .option('--pojo-dir <pojo-dir>', 'pojo path to generate')
+  .option('--pojo-package <package-name>', 'pojo package name')
+  .option('--ddl-dir <ddl-dir>', 'ddl path to generate')
+  .option('--dry', 'It will not create files, will only print to console')
+  .action(function () {
+    console.log("mysql-host : " + commander.mysqlHost);
+    console.log("mysql-port : " + commander.mysqlPort);
+    console.log("table-names : " + commander.tableNames);
+
+  })
+  .parse(process.argv);
+
